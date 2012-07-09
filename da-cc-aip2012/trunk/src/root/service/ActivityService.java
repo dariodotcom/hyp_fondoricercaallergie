e1a9c0d10e355eea7c06fe86ccd7bbbc5a3eb70b
dariodotcom <dariodotcom@84b2a735-b04f-4082-9e8d-97566b46fe4d>
2012-07-09 13:10:27 +0000

    Added documentation

--- da-cc-aip2012/trunk/src/root/service/ActivityService.java
+++ da-cc-aip2012/trunk/src/root/service/ActivityService.java
@@ -11,11 +11,18 @@ import root.model.Project;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 
+/**
+ * Utility class to retrieve/put Activities from/into the datastore.
+ * */
 public class ActivityService {
-    public static List<Activity> getActivityList(){
-        return Datastore.query(ActivityMeta.get()).asList();
-    }
-
+    /**
+     * Gives a list of Activity given the parameters of desired activities.
+     * 
+     * @parma type - the type of the search to perform
+     * @param value
+     *            - the optional value of the param to search
+     * @return the List of Activity
+     */
     public static List<Activity> getActivityList(String type, String value){
         ActivityMeta activityMeta = ActivityMeta.get();
         
@@ -23,17 +30,30 @@ public class ActivityService {
             Key projectKey = KeyFactory.stringToKey(value);
             Project linkedProject = Datastore.get(Project.class, projectKey);
             return linkedProject.getActivityRef().getModelList();
-        }else {
+        } else {
             return Datastore.query(activityMeta).sort(activityMeta.name.asc).asList();
         }
     }
-    
+
+    /**
+     * Retrieves an Activity from the Datastore given its key.
+     * 
+     * @param activityKey
+     *            - the Key to search
+     * @return the corresponding Activity
+     * */
     public static Activity get(Key activityKey) {
         Activity activity = Datastore.get(Activity.class, activityKey);
         return activity;
     }
-    
-    public static void put(Activity activity){
+
+    /**
+     * Puts an Activity into the Datastore
+     * 
+     * @param activity
+     *            - the Activity to put into the Datastore
+     * */
+    public static void put(Activity activity) {
         Datastore.put(activity);
     }
 }
